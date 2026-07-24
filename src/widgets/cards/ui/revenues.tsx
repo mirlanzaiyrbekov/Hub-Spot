20153815076218d158d11b2985e43c8053df0b81
mirlanzaiyrbekov <mirlan.zaiyrbekov@gmail.com>
2026-07-24 11:10:54 +0600

    completed

--- src/widgets/cards/ui/revenues.tsx
+++ src/widgets/cards/ui/revenues.tsx
@@ -1,4 +1,3 @@
-import { chartData } from "@/features"
 import {
 	Badge,
 	Card,
@@ -8,84 +7,66 @@ import {
 	CardFooter,
 	CardHeader,
 	CardTitle,
-	ChartContainer,
 } from "@/shared"
-import { TrendingUp } from "lucide-react"
+import {
+	ChevronRightIcon,
+	ChevronsUp,
+	FlagTriangleLeft,
+	TrendingUp,
+} from "lucide-react"
 import { FC } from "react"
-import { Area, AreaChart, CartesianGrid } from "recharts"
 
 export const Revenues: FC = () => {
 	return (
-		<Card className="@container/card shadow-xs rounded-md">
+		<Card className="@container/card shadow-xs rounded-md gap-3.5 py-3">
 			<CardHeader>
-				<CardDescription>Доход</CardDescription>
+				<CardDescription>Доходы</CardDescription>
 				<CardTitle className="text-2xl font-bold tabular-nums @[250px]/card:text-3xl">
-					18 500 сом
+					18 600 сом
 				</CardTitle>
 				<CardAction>
-					<Badge variant="outline" className="text-green-500">
+					<Badge variant="outline" className="text-green-600">
 						<TrendingUp className="size-4" />
 						+12%
 					</Badge>
 				</CardAction>
 			</CardHeader>
 			<CardContent>
-				<ChartContainer
-					config={{
-						revenue: {
-							label: "Прибыль",
-							color: "#5c1987",
-						},
-					}}
-					className="aspect-auto h-20 w-full"
-				>
-					<AreaChart data={chartData}>
-						<defs>
-							<linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-								<stop
-									offset="5%"
-									stopColor="var(--color-visitors)"
-									stopOpacity={1.0}
-								/>
-								<stop
-									offset="95%"
-									stopColor="var(--color-visitors)"
-									stopOpacity={0.1}
-								/>
-							</linearGradient>
-							<linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-								<stop
-									offset="5%"
-									stopColor="var(--color-revenue)"
-									stopOpacity={0.8}
-								/>
-								<stop
-									offset="95%"
-									stopColor="var(--color-revenue)"
-									stopOpacity={0.1}
-								/>
-							</linearGradient>
-						</defs>
-						<CartesianGrid vertical={false} />
-
-						<Area
-							dataKey="revenue"
-							type="natural"
-							fill="url(#fillMobile)"
-							stroke="var(--color-revenue)"
-							stackId="a"
-						/>
-					</AreaChart>
-				</ChartContainer>
+				<ul className="grid gap-1.5 text-sm">
+					<li className="flex items-center gap-1.5">
+						<ChevronRightIcon className="mt-0.5 size-2 shrink-0" />
+						<div className="w-full flex justify-between">
+							<span className="text-muted-foreground">За прошлый месяц:</span>
+							<b className=" flex items-center gap-1.5">
+								<ChevronsUp className="size-3 text-green-600" />
+								20.300 СОМ
+							</b>
+						</div>
+					</li>
+					<li className="flex items-center gap-1.5">
+						<ChevronRightIcon className="mt-0.5 size-2 shrink-0" />
+						<div className="w-full flex justify-between">
+							<span className="text-muted-foreground">
+								За сегодня: ожидаемый доход
+							</span>
+							<b>3.200 СОМ</b>
+						</div>
+					</li>
+					<li className="flex items-center gap-1.5">
+						<ChevronRightIcon className="mt-0.5 size-2 shrink-0" />
+						<div className="w-full flex justify-between">
+							<span className="text-muted-foreground">
+								Приблизительно за этот месяц:
+							</span>
+							<b className="flex items-center gap-1.5">
+								<FlagTriangleLeft className="size-2 text-green-600" />
+								23.303 СОМ
+							</b>
+						</div>
+					</li>
+				</ul>
 			</CardContent>
-			<CardFooter className="flex-col items-start gap-1 text-sm">
-				<div className="font-medium">
-					Сегодня: <span className="font-semibold">18 500 сом</span>
-				</div>
-				<div className="text-muted-foreground">
-					Вчера: 16 200 сом • Месяц: 420 000 сом
-				</div>
-			</CardFooter>
+			<CardFooter />
 		</Card>
 	)
 }
